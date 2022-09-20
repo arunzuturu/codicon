19b2fc8fff270ff3d0dbbd5d9e505a41cd100e0c
Arun Adithya <78028212+arunzuturu@users.noreply.github.com>
2022-09-20 20:25:23 +0530

    final changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mlr_app/Screens/Onboarding/landing.dart';
-import 'package:mlr_app/Screens/profile_form.dart';
 import 'package:mlr_app/shared_pref.dart';
 import 'Screens/navigation.dart';
 
